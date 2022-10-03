0f073a4ac3ce0e06f5b3a6189c9832851bc2f3e7
Mariah <mariahjohnston217@outlook.com>
2022-10-03 00:01:47 -0400

    started index.js file

---
+++ Assets/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bulma/dist/css/bulma.min.css';
+
+// unsure how to import font awesome or googlefonts 
+
+ReactDOM.render(<App />, document.getElementById('root'));
